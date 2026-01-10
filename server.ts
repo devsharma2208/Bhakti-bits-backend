019da56547743395c70561b27ec7e389c6c2e053
devsharma2208 <devsharmaelc@gmail.com>
2026-01-10 21:09:44 +0530

    add one more category

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import app from "./app.js";
 import { PORT } from "./config/env.js";
 
-app.listen(PORT, () => {
+app.listen(PORT || 4000, () => {
     console.log(`Playlist server running on http://localhost:${PORT}`);
 });
